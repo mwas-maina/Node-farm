39d3c59a6e6d890446d0945cf9dff6a5d5d889d7
mwas <emmamwas99@gmail.com>
2020-11-07 20:57:00 +0300

    last commit

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const replaceHtml=require("./modules/replace");
 const tempOverview=fs.readFileSync(`${__dirname}/templates/template-overview.html`,"utf-8");
 const tempProduct=fs.readFileSync(`${__dirname}/templates/template-product.html`,"utf-8");
 const tempCard=fs.readFileSync(`${__dirname}/templates/template-card.html`,"utf-8");
-
 const server=http.createServer((req,res)=>{
     const{pathname,query}=url.parse(req.url,true);
 
